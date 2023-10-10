04cce284ad11524c50eceba35248ac988c37f85b
keringKirwa <kkirwa230@gmail.com>
2023-10-10 09:39:39 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,12 @@
+restaurant_data = [
+    ["Yes", "Yes", "No", "Yes", "Yes", "No", "No", "No", "No", "Yes", "No", "Yes"],
+    ["No", "No", "Yes", "No", "No", "Yes", "Yes", "No", "Yes", "Yes", "No", "Yes"],
+    ["No", "No", "No", "Yes", "Yes", "No", "No", "No", "Yes", "Yes", "No", "Yes"],
+    ["Yes", "Yes", "No", "Yes", "No", "Yes", "No", "Yes", "No", "Yes", "No", "Yes"],
+    ["Some", "Full", "Some", "Full", "Full", "Some", "None", "Some", "Full", "Full", "None", "Full"],
+    ["$$$", "$", "$", "$", "$$$", "$$", "$", "$$", "$", "$$$", "$", "$$$"],
+    ["No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "No", "No", "No"],
+    ["Yes", "No", "No", "No", "Yes", "Yes", "No", "Yes", "No", "Yes", "No", "No"],
+    ["French", "Thai", "Burger", "Thai", "French", "Italian", "Burger", "Thai", "Burger", "Italian", "Thai", "Burger"],
+    ["0-10", "30-60", "0-10", "10-30", ">60", "0-10", "0-10", "0-10", ">60", "10-30", "0-10", "30-60"]
+]
